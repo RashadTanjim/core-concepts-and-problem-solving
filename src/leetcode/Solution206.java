d2807357c2958999ed34f820c05ad7de5317698e
Md. Rashad Tanjim <arnob.tanjim@gmail.com>
2025-05-15 01:23:25 +0600

    feat: add top practice problems - matrix, LinkedList

---
+++ src/leetcode/Solution206.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+import leetcode.helper.ListNode;
+
+public class Solution206 {
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode node = null;
+
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = node;
+            node = head;
+            head = temp;
+        }
+
+        return node;
+    }
+
+    public static void printList(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " → ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.print("Original List: ");
+        printList(head);
+
+        ListNode reversed = reverseList(head);
+        System.out.print("Reversed List: ");
+        printList(reversed);
+    }
+}
--- src/leetcode/Solution240.java
+++ src/leetcode/Solution240.java
@@ -3,4 +3,44 @@ package leetcode;
 import java.util.*;
 
 public class Solution240 {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
+
+        int row = 0;
+        int col = matrix[0].length - 1;
+
+        while (row < matrix.length && col >= 0) {
+            int value = matrix[row][col];
+            if (value == target) {
+                return true;
+            } else if (value > target) {
+                col--;  // Eliminate column
+            } else {
+                row++;  // Eliminate row
+            }
+        }
+
+        return false;
+    }
+
+
+//    public static boolean searchMatrix(int[][] matrix, int target) {
+//        for (int[] row : matrix) {
+//            int idx = Arrays.binarySearch(row, target);
+//            if (idx >= 0) return true;
+//        }
+//        return false;
+//    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 4, 7, 11, 15},
+                {2, 5, 8, 12, 19},
+                {3, 6, 9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}
+        };
+        int target = 5;
+        System.out.println(searchMatrix(matrix, target));  // true
+    }
 }
